08e9b8705ef98f35bcec552575c72482fb601f26
SebF33 <78741537+SebF33@users.noreply.github.com>
2022-10-26 23:05:10 +0200

    home update

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,8 +1,9 @@
+import { ArtistRow } from "@utils/types.tsx";
+import { colorScheme, currentColorScheme } from "@utils/colors.ts";
 import { Db } from "@utils/db.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
+import { sql } from "kysely";
 import { tw } from "@twind";
-
-import { ArtistRow } from "@utils/types.tsx";
 
 import ArtistsLayout from "@components/ArtistsLayout.tsx";
 import Footer from "@islands/Footer.tsx";
@@ -13,39 +14,62 @@ type Artists = Array<ArtistRow>;
 
 export const handler: Handlers<{
   artists: Artists;
+  color: string;
+  grid: string;
 }> = {
   async GET(_, ctx) {
     const db = Db.getInstance();
-
     const results = await db.selectFrom("artist").selectAll()
-      .limit(4).orderBy("last_name").execute();
+      .orderBy(sql`random()`)
+      .limit(4)
+      .execute();
 
     const artists = results.map((p) => ({
       id: p.id,
-      firstName: p.first_name,
-      lastName: p.last_name,
-      avatar: p.avatar_url,
+      first_name: p.first_name,
+      last_name: p.last_name,
+      avatar_url: p.avatar_url,
       signature: p.signature,
       slug: p.slug,
     }));
 
-    return ctx.render({ artists });
+    const randomIndex = Math.floor(Math.random() * 8);
+    const colors = [
+      colorScheme[currentColorScheme].lighterdark,
+      colorScheme[currentColorScheme].red,
+      colorScheme[currentColorScheme].green,
+      colorScheme[currentColorScheme].yellow,
+      colorScheme[currentColorScheme].blue,
+      colorScheme[currentColorScheme].magenta,
+      colorScheme[currentColorScheme].cyan,
+      colorScheme[currentColorScheme].gray,
+    ];
+    const color = colors[randomIndex];
+
+    const grid =
+      "grid gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 pt-10 pb-10 lg:pt-20 lg:pb-20";
+
+    return ctx.render({ artists, color, grid });
   },
 };
 
 export default function Home(
   props: PageProps<{
     artists: Artists;
+    color: string;
+    grid: string;
   }>,
 ) {
-  const { artists } = props.data;
+  const { artists, color, grid } = props.data;
 
   return (
     <div class={tw`flex flex-col min-h-screen`}>
       <Header />
-      <ArtistsLayout artists={artists} />
-      <WaterDrop />
-      <Footer />
+      <main class={tw`flex-grow`}>
+        <ArtistsLayout artists={artists} grid={grid} />
+      </main>
+      <WaterDrop color={color} />
+      <Footer color={color} />
     </div>
   );
 }
