12a9afc83838ea1ad281d9688f17b678f42acdcd
SebF33 <78741537+SebF33@users.noreply.github.com>
2022-10-28 22:11:34 +0200

    everblush colors

---
+++ utils/colors.ts
@@ -0,0 +1,16 @@
+export const colorScheme = {
+  everblush: {
+    dark: "#141b1e",
+    lighterdark: "#232a2d",
+    red: "#e57474",
+    green: "#8ccf7e",
+    yellow: "#e5c76b",
+    blue: "#67b0e8",
+    magenta: "#c47fd5",
+    cyan: "#6cbfbf",
+    gray: "#b3b9b8",
+    white: "#dadada",
+  },
+};
+
+export const currentColorScheme = "everblush";
