84c2f41285de1212580d882ee828bc9d7e20d154
SebF33 <78741537+SebF33@users.noreply.github.com>
2023-07-25 00:13:05 +0200

    fontSize

--- utils/twind.ts
+++ utils/twind.ts
@@ -17,6 +17,16 @@ export const config: Configuration = {
         street: "Five Boroughs Handwriting",
       },
     },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
     screens: {
       xs: "300px",
       sm: "480px",
