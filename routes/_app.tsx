e0766754dcd207c593f88e7fa815150ec9286997
SebF33 <78741537+SebF33@users.noreply.github.com>
2023-08-19 02:43:38 +0200

    noUiSlider

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,6 +5,8 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
+        <link href={asset("/styles/lib/nouislider.min.css")} rel="stylesheet" />
+        <script src="/styles/lib/nouislider.min.js"></script>
         <link href={asset("/styles/lib/tippy6.3.7.css")} rel="stylesheet" />
         <link href={asset("/styles/style.css")} rel="stylesheet" />
       </Head>
