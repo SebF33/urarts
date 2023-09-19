b4a2e643cc560e54acbfddf403e5c4963aa982a8
SebF33 <78741537+SebF33@users.noreply.github.com>
2023-09-19 22:12:13 +0200

    no results placeholder

--- islands/layout/ArtistsLayout.tsx
+++ islands/layout/ArtistsLayout.tsx
@@ -12,7 +12,18 @@ type Artists = Array<ArtistRow>;
 export default function ArtistsLayout(
   props: { artists: Artists; grid: string },
 ) {
+  const [showPlaceholder, setShowPlaceholder] = useState(false);
   const [tippyInstances, setTippyInstances] = useState<Any[]>([]);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowPlaceholder(true);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
 
   useEffect(() => {
     tippyInstances.forEach((instance) => {
@@ -57,20 +68,18 @@ export default function ArtistsLayout(
 
   return (
     <div class={tw`max-w-7xl mx-auto px-6 sm:px-8 md:px-10 lg:px-12`}>
-      {props.artists &&
-        (
-          <div
-            class={tw`${props.grid}`}
-          >
+      {props.artists && props.artists.length > 0
+        ? (
+          <div class={tw`${props.grid}`}>
             {props.artists.map((p) => (
               <div
                 data-artist-id={p.id}
                 class={tw`artist-frame ${
                   css(
                     {
-                      "position": "relative",
+                      position: "relative",
                       "padding-bottom": "120%",
-                      "background": "black",
+                      background: "black",
                       "box-shadow": "0 10px 7px -5px rgba(0, 0, 0, 0.3)",
                     },
                   )
@@ -80,11 +89,11 @@ export default function ArtistsLayout(
                   class={tw`font-brush z-10 ${
                     css(
                       {
-                        "position": "absolute",
-                        "top": "6.0303%",
-                        "bottom": "3.0303%",
-                        "left": "10.5%",
-                        "right": "2.5%",
+                        position: "absolute",
+                        top: "6.0303%",
+                        bottom: "3.0303%",
+                        left: "10.5%",
+                        right: "2.5%",
                         "font-size": "calc(0.8em + 0.16vw)",
                       },
                     )
@@ -92,33 +101,32 @@ export default function ArtistsLayout(
                 >
                   {p.first_name} {p.last_name}
                 </p>
-                {p.signature &&
-                  (
-                    <img
-                      class={tw`w-12 z-10 ${
-                        css(
-                          {
-                            "position": "absolute",
-                            "top": "86.0303%",
-                            "left": "70.5%",
-                            "font-size": "calc(0.6em + 0.5vw)",
-                          },
-                        )
-                      }`}
-                      src={p.signature}
-                      alt={p.signature}
-                    />
-                  )}
+                {p.signature && (
+                  <img
+                    class={tw`w-12 z-10 ${
+                      css(
+                        {
+                          position: "absolute",
+                          top: "86.0303%",
+                          left: "70.5%",
+                          "font-size": "calc(0.6em + 0.5vw)",
+                        },
+                      )
+                    }`}
+                    src={p.signature}
+                    alt={p.signature}
+                  />
+                )}
                 <div
                   class={tw`${
                     css(
                       {
-                        "position": "absolute",
-                        "background": "white",
-                        "top": "3.0303%",
-                        "bottom": "3.0303%",
-                        "left": "2.5%",
-                        "right": "2.5%",
+                        position: "absolute",
+                        background: "white",
+                        top: "3.0303%",
+                        bottom: "3.0303%",
+                        left: "2.5%",
+                        right: "2.5%",
                         "box-shadow":
                           "0px 0px 20px 0px rgba(0, 0, 0, 0.5) inset",
                       },
@@ -131,18 +139,18 @@ export default function ArtistsLayout(
                     class={tw`group flex justify-center text-center relative overflow-hidden z-20 cursor-pointer ${
                       css(
                         {
-                          "position": "absolute",
-                          "top": "16.129%",
-                          "bottom": "16.129%",
-                          "left": "13.158%",
-                          "right": "13.158%",
+                          position: "absolute",
+                          top: "16.129%",
+                          bottom: "16.129%",
+                          left: "13.158%",
+                          right: "13.158%",
                           "&::after": {
                             content: "",
-                            "display": "block",
-                            "position": "absolute",
-                            "top": "0",
-                            "width": "100%",
-                            "height": "100%",
+                            display: "block",
+                            position: "absolute",
+                            top: "0",
+                            width: "100%",
+                            height: "100%",
                             "box-shadow":
                               "0px 0px 20px 0px rgba(0, 0, 0, 0.5) inset",
                           },
@@ -174,6 +182,73 @@ export default function ArtistsLayout(
                 </ul>
               </div>
             ))}
+          </div>
+        )
+        : (
+          <div class={tw`${props.grid}`}>
+            {showPlaceholder
+              ? (
+                <div
+                  class={tw`${
+                    css(
+                      {
+                        "position": "relative",
+                        "padding-bottom": "120%",
+                        "background": "black",
+                        "box-shadow": "0 10px 7px -5px rgba(0, 0, 0, 0.3)",
+                      },
+                    )
+                  }`}
+                >
+                  <div
+                    class={tw`${
+                      css(
+                        {
+                          "position": "absolute",
+                          "background": "white",
+                          "top": "3.0303%",
+                          "bottom": "3.0303%",
+                          "left": "2.5%",
+                          "right": "2.5%",
+                          "box-shadow":
+                            "0px 0px 20px 0px rgba(0, 0, 0, 0.5) inset",
+                        },
+                      )
+                    }`}
+                  >
+                    <div
+                      class={tw`group flex justify-center text-center relative overflow-hidden z-20 ${
+                        css(
+                          {
+                            "position": "absolute",
+                            "top": "16.129%",
+                            "bottom": "16.129%",
+                            "left": "13.158%",
+                            "right": "13.158%",
+                            "&::after": {
+                              content: "",
+                              "display": "block",
+                              "position": "absolute",
+                              "top": "0",
+                              "width": "100%",
+                              "height": "100%",
+                              "box-shadow":
+                                "0px 0px 20px 0px rgba(0, 0, 0, 0.5) inset",
+                            },
+                          },
+                        )
+                      }`}
+                    >
+                      <img
+                        class={tw`w-full object-cover`}
+                        src="/errors/0.jpg"
+                        alt="Pas de résultats"
+                      />
+                    </div>
+                  </div>
+                </div>
+              )
+              : null}
           </div>
         )}
     </div>
