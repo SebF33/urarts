995429d9fdf1a8d9bb1163121853bcb78e819f70
SebF33 <78741537+SebF33@users.noreply.github.com>
2025-01-13 22:26:40 +0100

    refacto & readability

--- routes/art/[slug].tsx
+++ routes/art/[slug].tsx
@@ -1,6 +1,6 @@
 import { ArtistQuote } from "@utils/types.d.ts";
 import { Db } from "@utils/db.ts";
-import DOMPurify from "npm:isomorphic-dompurify"
+import DOMPurify from "npm:isomorphic-dompurify";
 import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
 import { Head, Partial } from "$fresh/runtime.ts";
 import i18next from "i18next";
@@ -15,151 +15,134 @@ import WaterDrop from "@islands/footer/WaterDrop.tsx";
 
 type Quote = Array<ArtistQuote>;
 
+interface Movement {
+  font: string;
+  movementName: string;
+  movementSlug: string;
+  position: string;
+}
 
-export const handler: Handlers = {
+interface QueryParameters {
+  alone: boolean;
+  fromLeonardo: boolean;
+  id: string;
+}
+
+interface ArtistPageProps {
+  artist: string;
+  artistQuote: Quote | null;
+  avatar: string;
+  avatarInfo: string;
+  birthyear: string;
+  color: string;
+  copyright: number;
+  deathyear: string;
+  description: string;
+  info: string;
+  movements: Movement[];
+  nationality: string;
+  queryParameters: QueryParameters;
+  secondaryColor: string;
+  site: string;
+  slug: string;
+  title: string;
+}
+
+
+export const handler: Handlers<ArtistPageProps> = {
   async GET(req: Request, ctx: FreshContext) {
     const lng = i18next.language;
     const { slug } = ctx.params;
     const url = new URL(req.url);
 
     const db = Db.getInstance();
-    const result = await db.selectFrom("artist")
+
+    const artistDetails = await db
+      .selectFrom("artist")
       .innerJoin("country", "artist.country_id", "country.id")
       .select([
         "first_name", "last_name",
         "avatar_url", "color", "secondary_color", "site_web",
-        "birthyear", "deathyear",
-        "signature", "quote",
-        "copyright",
-        "slug",
+        "birthyear", "deathyear", "signature", "quote", "copyright", "slug",
       ])
-      .$if(lng === 'fr', (qb) => qb.select("avatar_info"))
-      .$if(lng === 'en', (qb) => qb.select("avatar_info_en as avatar_info"))
-      .$if(lng === 'fr', (qb) => qb.select("info"))
-      .$if(lng === 'en', (qb) => qb.select("info_en as info"))
-      .$if(lng === 'fr', (qb) => qb.select("country.name as nationality"))
-      .$if(lng === 'en', (qb) => qb.select("country.name_en as nationality"))
+      .$if(lng === 'fr', (query) => query.select("avatar_info"))
+      .$if(lng === 'en', (query) => query.select("avatar_info_en as avatar_info"))
+      .$if(lng === 'fr', (query) => query.select("info"))
+      .$if(lng === 'en', (query) => query.select("info_en as info"))
+      .$if(lng === 'fr', (query) => query.select("country.name as nationality"))
+      .$if(lng === 'en', (query) => query.select("country.name_en as nationality"))
       .where("slug", "=", slug)
       .executeTakeFirst();
 
-    let artist: string | null = null;
-    let artistQuote: Quote | null = null;
-    let avatar: string | null = null;
-    let avatarInfo: string | null = null;
-    let birthyear: string | null = null;
-    let color: string | null = null;
-    let copyright: number | null = null;
-    let deathyear: string | null = null;
-    let desc: string | null = null;
-    let info: string | null = null;
-    let movements: string[] | null = null;
-    let mySlug: string | null = null;
-    let nationality: string | null = null;
-    let query: object | null = null;
-    let secondaryColor: string | null = null;
-    let site: string | null = null;
-    let title: string | null = null;
+    if (!artistDetails) {
+      return ctx.renderNotFound();
+    }
 
-    if (result) {
-      const alone = url.searchParams.has("alone");
-      const fromLeonardo = url.searchParams.has("fromleonardo");
-      const id = url.searchParams.get("id") || "";
+    const searchParams = url.searchParams;
+    const queryParameters: QueryParameters = {
+      alone: searchParams.has("alone"),
+      fromLeonardo: searchParams.has("fromleonardo"),
+      id: searchParams.get("id") || "",
+    };
 
-      const movementQuery = await db.selectFrom("art")
+    const movementQuery = await db
+      .selectFrom("art")
       .innerJoin("artist", "art.owner_id", "artist.id")
       .innerJoin("movement", "art.movement_id", "movement.id")
       .select(["movement.font", "movement.slug"])
-      .$if(lng === 'fr', (qb) => qb.select("movement.name"))
-      .$if(lng === 'en', (qb) => qb.select("movement.name_en as name"))
+      .$if(lng === 'fr', (query) => query.select("movement.name"))
+      .$if(lng === 'en', (query) => query.select("movement.name_en as name"))
       .distinct()
-      .where("artist.slug", "=", result.slug)
+      .where("artist.slug", "=", artistDetails.slug)
       .where("movement.slug", "!=", "nonclasse")
-      .$if(lng === 'fr', (qb) => qb.orderBy("movement.name"))
-      .$if(lng === 'en', (qb) => qb.orderBy("movement.name_en"))
+      .$if(lng === 'fr', (query) => query.orderBy("movement.name"))
+      .$if(lng === 'en', (query) => query.orderBy("movement.name_en"))
       .execute();
-      
-      // Position des étiquettes de mouvement artistique
-      const marginValues = ["-ml-2", "ml-6", "-ml-1", "ml-12", "-ml-3", "ml-2"];
-      const rotationValues = ["-rotate-3", "rotate-3", "-rotate-1", "rotate-2", "-rotate-2", "rotate-6"];
 
-      artist = result.first_name !== null
-        ? result.first_name + " " + result.last_name
-        : result.last_name;
-        
-      if (result.quote !== null) artistQuote = {first_name: result.first_name, last_name: result.last_name, signature: result.signature, quote: result.quote};
-      else artistQuote = null;
+    const movementLabels: Movement[] = movementQuery.map((movement, index) => {
+      const marginClasses = ["-ml-2", "ml-6", "-ml-1", "ml-12", "-ml-3", "ml-2"];
+      const rotationClasses = ["-rotate-3", "rotate-3", "-rotate-1", "rotate-2", "-rotate-2", "rotate-6"];
 
-      avatar = result.avatar_url;
-      avatarInfo = result.avatar_info;
-      birthyear = result.birthyear;
-      color = result.color;
-      copyright = result.copyright;
-      deathyear = result.deathyear !== "" ? " — " + result.deathyear : "";
-      desc = i18next.t("meta.collection.desc", { ns: "translation" }) + " " + artist + ".";
-      info = result.info;
-      movements = movementQuery.map((p, index) => ({
-        font: p.font,
-        movement_name: p.name,
-        position: marginValues[index % marginValues.length] + " " + rotationValues[index % rotationValues.length],
-        movement_slug: p.slug,
-      }));
-      mySlug = result.slug;
-      nationality = result.nationality;
-      query = {
-        alone: alone,
-        fromLeonardo: fromLeonardo,
-        id: id
+      return {
+        font: movement.font,
+        movementName: movement.name ?? "",
+        movementSlug: movement.slug,
+        position: `${marginClasses[index % marginClasses.length]} ${rotationClasses[index % rotationClasses.length]}`,
       };
-      secondaryColor = result.secondary_color;
-      site = result.site_web;
-      title = artist + " " + i18next.t("meta.collection.title", { ns: "translation" });
-    } else return ctx.renderNotFound();
+    });
+
+    const artist = artistDetails.first_name ? `${artistDetails.first_name} ${artistDetails.last_name}` : artistDetails.last_name;
+    const description = `${i18next.t("meta.collection.desc", { ns: "translation" })} ${artist}.`;
 
     return ctx.render({
       artist,
-      artistQuote,
-      avatar,
-      avatarInfo,
-      birthyear,
-      color,
-      copyright,
-      deathyear,
-      desc,
-      info,
-      movements,
-      mySlug,
-      nationality,
-      query,
-      secondaryColor,
-      site,
-      title,
+      artistQuote: artistDetails.quote ? {
+        first_name: artistDetails.first_name,
+        last_name: artistDetails.last_name,
+        signature: artistDetails.signature,
+        quote: artistDetails.quote,
+      } : null,
+      avatar: artistDetails.avatar_url,
+      avatarInfo: artistDetails.avatar_info,
+      birthyear: artistDetails.birthyear,
+      color: artistDetails.color,
+      copyright: artistDetails.copyright,
+      deathyear: artistDetails.deathyear ? ` — ${artistDetails.deathyear}` : "",
+      description,
+      info: artistDetails.info,
+      movements: movementLabels,
+      nationality: artistDetails.nationality,
+      queryParameters,
+      secondaryColor: artistDetails.secondary_color,
+      site: artistDetails.site_web,
+      slug: artistDetails.slug,
+      title: `${artist} ${i18next.t("meta.collection.title", { ns: "translation" })}`,
     });
   },
 };
 
 
-export default function ArtistArtPage(
-  props: PageProps<{
-    artist: string;
-    artistQuote: Quote | null;
-    avatar: string;
-    avatarInfo: string;
-    birthyear: string;
-    color: string;
-    copyright: number;
-    deathyear: string;
-    desc: string;
-    info: string;
-    movements: string[];
-    mySlug: string;
-    nationality: string;
-    query: object;
-    secondaryColor: string;
-    site: string;
-    title: string;
-  }>,
-) {
-  
+export default function ArtistArtPage(props: PageProps<ArtistPageProps>) {
   const {
     artist,
     artistQuote,
@@ -169,164 +152,117 @@ export default function ArtistArtPage(
     color,
     copyright,
     deathyear,
-    desc,
+    description,
     info,
     movements,
-    mySlug,
     nationality,
-    query,
+    queryParameters,
     secondaryColor,
     site,
+    slug,
     title,
   } = props.data;
 
   const draggable = false;
 
+
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content={desc} />
+        <meta name="description" content={description} />
         <meta property="og:title" content={title} />
-        <meta property="og:description" content={desc} />
+        <meta property="og:description" content={description} />
         <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={desc} />
+        <meta name="twitter:description" content={description} />
       </Head>
 
-      <main id="page" data-name="collection" class="scrollable flex-grow mb-6 xl:max-h-screen xl:overflow-y-scroll custom-scrollbar transparent-mask-99">
-        <div
-          class={`relative w-auto flex flex-col mx-auto`}
-        >
-          {movements &&
-            <div class={`invisible md:visible absolute mt-12 ml-16`}>
-              {movements && movements.map((p) => (
-                <div
-                  class={`paper appear-effect-fast-fadein max-w-[140px] min-h-8 mt-1 ${p.position} font-${p.font} shadow-none`}
-                >
+      <main
+        id="page"
+        data-name="collection"
+        class="scrollable flex-grow mb-6 xl:max-h-screen xl:overflow-y-scroll custom-scrollbar transparent-mask-99"
+      >
+        <div class="relative w-auto flex flex-col mx-auto">
+          {movements.length > 0 && (
+            <div class="invisible md:visible absolute mt-12 ml-16">
+              {movements.map((movement) => (
+                <div class={`paper appear-effect-fast-fadein max-w-[140px] min-h-8 mt-1 ${movement.position} font-${movement.font} shadow-none`}>
                   <div class="top-tape max-h-3"></div>
-                  <a
-                    href={"/movement/" + p.movement_slug}
-                    class={`z-10 text-lighterdark text-xl italic underline select-none`}
-                    draggable={draggable}
-                  >
-                    {p.movement_name}
+                  <a href={`/movement/${movement.movementSlug}`} class="z-10 text-lighterdark text-xl italic underline select-none" draggable={draggable}>
+                    {movement.movementName}
                   </a>
                 </div>
               ))}
             </div>
-          }
+          )}
 
-          <div class={`mx-auto mt-8 z-10`}>
-            <AnimBrushStroke
-              color={color}
-              font="brush"
-              secondaryColor={secondaryColor}
-              title={artist}
-            />
+          <div class="mx-auto mt-8 z-10">
+            <AnimBrushStroke color={color} font="brush" secondaryColor={secondaryColor} title={artist} />
           </div>
-          <div class={`-mt-44`}>
-            <div
-              class={`h-[38rem] md:h-96 bg-lighterdark shadow-2xl`}
-            >
-            </div>
 
-            <div
-              class={`-mt-[27rem] md:-mt-[13.5rem] text-white`}
-            >
-              <div
-                class={`w-11/12 xl:w-[38rem] mx-auto text-center`}
-              >
-                <p class="font-bold italic text-xl">
-                  {birthyear + deathyear}
-                </p>
-                <img
-                  class={`inline-block w-12 mt-1`}
-                  src={"/flags/" + nationality + ".png"}
-                  alt={nationality}
-                  draggable={draggable}
-                />
+          <div class="-mt-44">
+            <div class="h-[38rem] md:h-96 bg-lighterdark shadow-2xl"></div>
+            <div class="-mt-[27rem] md:-mt-[13.5rem] text-white">
+              <div class="w-11/12 xl:w-[38rem] mx-auto text-center">
+                <p class="font-bold italic text-xl">{birthyear + deathyear}</p>
+                <img class="inline-block w-12 mt-1" src={`/flags/${nationality}.png`} alt={nationality} draggable={draggable} />
                 <p class="font-bold text-lg mb-2">
                   {i18next.t("artists.nationality", { ns: "translation" }) + " " + nationality}
                 </p>
-                <p class={`relative text-[1.1rem] text-justify leading-[1.12rem] select-none z-10`} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(info) }}></p>
+                <p class="relative text-[1.1rem] text-justify leading-[1.12rem] select-none z-10" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(info) }}></p>
               </div>
 
-              {site &&
-                (
-                  <div class={`relative w-11/12 mt-3 sm:mt-2`}>
-                    <div class="paper min-h-8 max-w-[230px] ml-auto z-10 shadow-none">
-                      <div class="top-tape"></div>
-                      <a
-                        href={site}
-                        class={`text-lighterdark text-base italic underline z-10 select-none`}
-                        target="_blank"
-                        rel="noopener"
-                        draggable={draggable}
-                      >
-                        {site}
-                      </a>
-                    </div>
+              {site && (
+                <div class="relative w-11/12 mt-3 sm:mt-2">
+                  <div class="paper min-h-8 max-w-[230px] ml-auto z-10 shadow-none">
+                    <div class="top-tape"></div>
+                    <a href={site} class="text-lighterdark text-base italic underline z-10 select-none" target="_blank" rel="noopener" draggable={draggable}>
+                      {site}
+                    </a>
                   </div>
-                )}
+                </div>
+              )}
 
-              {avatar &&
-                (
-                  <div
-                    class={`-mt-12 xl:-mt-40 grid grid-cols-1 xl:grid-cols-3`}
-                  >
-                    <div class={`pt-16 sm:pt-12 md:pt-10 sm:pl-12 sm:pr-12`}>
-                      <Avatar copyright={copyright} info={avatarInfo} name={artist} url={avatar} />
-                    </div>
+              {avatar && (
+                <div class="-mt-12 xl:-mt-40 grid grid-cols-1 xl:grid-cols-3">
+                  <div class="pt-16 sm:pt-12 md:pt-10 sm:pl-12 sm:pr-12">
+                    <Avatar copyright={copyright} info={avatarInfo} name={artist} url={avatar} />
                   </div>
-                )}
+                </div>
+              )}
             </div>
           </div>
 
-          {artistQuote && !query?.alone && (
+          {artistQuote && !queryParameters.alone && (
             <div class="w-full mx-auto mt-8 2xl:-mt-8 mb-4">
               <Quote data={artistQuote} delay={10} />
             </div>
           )}
 
-          {copyright != 2 && (
+          {copyright !== 2 ? (
             <Partial name="artist-collection">
-              <CollectionSearch query={query} myslug={mySlug} type="artist" />
+              <CollectionSearch query={queryParameters} myslug={slug} type="artist" />
             </Partial>
-          )}
-
-          {copyright === 2 &&
-            (
-              <div class={`flex-grow`}>
-                <div
-                  class={`max-w-2xl mx-auto py-10 px-6 mt-5`}
-                >
-                  <div
-                    class={`paper max-w-[600px] mx-auto`}
-                  >
-                    <div class="top-tape"></div>
-                    <div
-                      class={`w-full my-3 mx-1`}
-                    >
-                      <p
-                        class={`text-2xl md:text-3xl font-extrabold leading-5 text-center text-lighterdark`}
-                      >
-                        <span class={`text-7xl md:text-8xl`}>©</span>
-                        <br />
-                        {i18next.t("paper.copyright", { ns: "translation" })}
-                      </p>
-                    </div>
+          ) : (
+            <div class="flex-grow">
+              <div class="max-w-2xl mx-auto py-10 px-6 mt-5">
+                <div class="paper max-w-[600px] mx-auto">
+                  <div class="top-tape"></div>
+                  <div class="w-full my-3 mx-1">
+                    <p class="text-2xl md:text-3xl font-extrabold leading-5 text-center text-lighterdark">
+                      <span class="text-7xl md:text-8xl">©</span>
+                      <br />
+                      {i18next.t("paper.copyright", { ns: "translation" })}
+                    </p>
                   </div>
                 </div>
               </div>
-            )}
+            </div>
+          )}
         </div>
       </main>
 
-      <WaterDrop
-        color={color}
-        isDropy
-        pencilColor={secondaryColor}
-      />
+      <WaterDrop color={color} isDropy pencilColor={secondaryColor} />
       <Footer color={color} />
     </>
   );
--- routes/movement/[slug].tsx
+++ routes/movement/[slug].tsx
@@ -1,6 +1,6 @@
 import { colorScheme, currentColorScheme } from "@utils/colors.ts";
 import { Db } from "@utils/db.ts";
-import DOMPurify from "npm:isomorphic-dompurify"
+import DOMPurify from "npm:isomorphic-dompurify";
 import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
 import { Head, Partial } from "$fresh/runtime.ts";
 import i18next from "i18next";
@@ -13,96 +13,90 @@ import CollectionSearch from "@islands/livesearch/CollectionSearch.tsx";
 import Footer from "@islands/footer/Footer.tsx";
 import WaterDrop from "@islands/footer/WaterDrop.tsx";
 
+interface Artist {
+  avatar_url: string;
+  name: string;
+  position: string;
+  slug: string;
+}
 
-export const handler: Handlers = {
+interface MovementPageProps {
+  artists: Artist[];
+  desc: string;
+  font: string;
+  info: string;
+  movement: string;
+  slug: string;
+  title: string;
+}
+
+
+export const handler: Handlers<MovementPageProps> = {
   async GET(_: Request, ctx: FreshContext) {
     const lng = i18next.language;
     const { slug } = ctx.params;
 
     const db = Db.getInstance();
-    const result = await db.selectFrom("movement")
+
+    const movementDetails = await db
+      .selectFrom("movement")
       .select(["font", "slug"])
       .$if(lng === 'fr', (qb) => qb.select("name"))
       .$if(lng === 'en', (qb) => qb.select("name_en as name"))
       .$if(lng === 'fr', (qb) => qb.select("info"))
       .$if(lng === 'en', (qb) => qb.select("info_en as info"))
-      .where("slug", "=", slug).executeTakeFirst();
+      .where("slug", "=", slug)
+      .executeTakeFirst();
 
-    let artists: string[] | null = null;
-    let desc: string | null = null;
-    let font: string | null = null;
-    let info: string | null = null;
-    let movement: string | null = null;
-    let mySlug: string | null = null;
-    let title: string | null = null;
+    if (!movementDetails) {
+      return ctx.renderNotFound();
+    }
 
-    if (result) {
-      const artistQuery = await db.selectFrom("art")
+    const artistQuery = await db
+      .selectFrom("art")
       .innerJoin("artist", "art.owner_id", "artist.id")
       .innerJoin("movement", "art.movement_id", "movement.id")
       .select(["artist.avatar_url", "artist.last_name", "artist.slug"])
       .distinct()
-      .where("movement.slug", "=", result.slug)
+      .where("movement.slug", "=", movementDetails.slug)
       .where("artist.slug", "not in", TALENTS)
       .orderBy(sql`random()`)
       .limit(12)
       .execute();
-      
-      // Position des étiquettes d'artiste
-      const lValues = ["-ml-2", "ml-36", "-ml-1", "ml-40", "-ml-3", "ml-2", "-ml-2", "ml-1", "-ml-2", "ml-40", "-ml-3", "ml-36"];
-      const rValues = ["-rotate-3", "rotate-6", "rotate-2", "-rotate-2", "-rotate-2", "rotate-6", "-rotate-3", "rotate-3", "rotate-0", "rotate-3", "rotate-1", "-rotate-12"];
-      const tValues = ["-mt-2", "-mt-12", "-mt-1", "-mt-14", "-mt-3", "mt-2", "-mt-2", "mt-1", "-mt-2", "-mt-24", "mt-6", "-mt-16"];
 
-      artists = artistQuery.map((p, index) => ({
-        avatar_url: p.avatar_url,
-        name: p.last_name,
-        position: tValues[index % tValues.length] + " " + lValues[index % lValues.length] + " " + rValues[index % rValues.length],
-        slug: p.slug,
-      }));
-      movement = result.name;
-      desc = movement + ".";
-      font = result.font;
-      info = result.info;
-      mySlug = result.slug;
-      title = movement + " " + i18next.t("meta.collection.title", { ns: "translation" });
-    } else return ctx.renderNotFound();
+    const lValues = ["-ml-2", "ml-36", "-ml-1", "ml-40", "-ml-3", "ml-2", "-ml-2", "ml-1", "-ml-2", "ml-40", "-ml-3", "ml-36"];
+    const rValues = ["-rotate-3", "rotate-6", "rotate-2", "-rotate-2", "-rotate-2", "rotate-6", "-rotate-3", "rotate-3", "rotate-0", "rotate-3", "rotate-1", "-rotate-12"];
+    const tValues = ["-mt-2", "-mt-12", "-mt-1", "-mt-14", "-mt-3", "mt-2", "-mt-2", "mt-1", "-mt-2", "-mt-24", "mt-6", "-mt-16"];
+
+    const artists = artistQuery.map((p, index) => ({
+      avatar_url: p.avatar_url,
+      name: p.last_name,
+      position: `${tValues[index % tValues.length]} ${lValues[index % lValues.length]} ${rValues[index % rValues.length]}`,
+      slug: p.slug,
+    }));
+
+    const movement = movementDetails.name;
+    const desc = `${movement}.`;
+    const title = `${movement} ${i18next.t("meta.collection.title", { ns: "translation" })}`;
 
     return ctx.render({
       artists,
       desc,
-      font,
-      info,
+      font: movementDetails.font,
+      info: movementDetails.info,
       movement,
-      mySlug,
+      slug: movementDetails.slug,
       title,
     });
   },
 };
 
 
-export default function MovementArtsPage(
-  props: PageProps<{
-    artists: string[];
-    desc: string;
-    font: string;
-    info: string;
-    movement: string;
-    mySlug: string;
-    title: string;
-  }>,
-) {
-
-  const {
-    artists,
-    desc,
-    font,
-    info,
-    movement,
-    mySlug,
-    title,
-  } = props.data;
+export default function MovementArtsPage(props: PageProps<MovementPageProps>) {
+  const { artists, desc, font, info, movement, slug, title } = props.data;
 
   const draggable = false;
+
 
   return (
     <>
@@ -115,33 +109,33 @@ export default function MovementArtsPage(
         <meta name="twitter:description" content={desc} />
       </Head>
 
-      <main id="page" data-name="collection" class="scrollable flex-grow mb-6 xl:max-h-screen xl:overflow-y-scroll custom-scrollbar transparent-mask-99">
-        <div
-          class={`relative w-auto flex flex-col mx-auto`}
-        >
-          {artists &&
+      <main
+        id="page"
+        data-name="collection"
+        class="scrollable flex-grow mb-6 xl:max-h-screen xl:overflow-y-scroll custom-scrollbar transparent-mask-99"
+      >
+        <div class="relative w-auto flex flex-col mx-auto">
+          {artists.length > 0 && (
             <Partial name="artists-paper">
-              <div class={`invisible xl:visible absolute max-w-0 xl:max-w-full mt-12 ml-16 overflow-hidden xl:overflow-visible`}>
-                {artists && artists.map((p) => (
-                  <div
-                    class={`paper appear-effect-fast-fadein max-w-[140px] min-h-8 ${p.position} shadow-none`}
-                  >
+              <div class="invisible xl:visible absolute max-w-0 xl:max-w-full mt-12 ml-16 overflow-hidden xl:overflow-visible">
+                {artists.map((artist) => (
+                  <div class={`paper appear-effect-fast-fadein max-w-[140px] min-h-8 ${artist.position} shadow-none`}>
                     <div class="top-tape max-h-3"></div>
                     <a
-                      href={"/art/" + p.slug}
-                      class={`z-10 text-center text-lighterdark text-xl italic underline select-none`}
+                      href={`/art/${artist.slug}`}
+                      class="z-10 text-center text-lighterdark text-xl italic underline select-none"
                       draggable={draggable}
                     >
-                      {p.name}
+                      {artist.name}
                     </a>
-                    <img class={`w-14 ml-3 p-1`} src={p.avatar_url} alt={p.name} />
+                    <img class="w-14 ml-3 p-1" src={artist.avatar_url} alt={artist.name} />
                   </div>
                 ))}
               </div>
             </Partial>
-          }
+          )}
 
-          <div class={`mx-auto mt-8 z-10`}>
+          <div class="mx-auto mt-8 z-10">
             <AnimBrushStroke
               color={colorScheme[currentColorScheme].white}
               font={font}
@@ -149,19 +143,19 @@ export default function MovementArtsPage(
               title={movement}
             />
           </div>
-          <div class={`-mt-44`}>
-            <div
-              class={`h-fit bg-lighterdark shadow-2xl`}
-            >
-              <div
-                class={`w-11/12 xl:w-3/6 mx-auto pt-48 text-center`}
-              >
-                <p class={`relative pb-8 text-center text-[1.1rem] text-white text-justify leading-5 z-10 select-none`} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(info) }}></p>
+
+          <div class="-mt-44">
+            <div class="h-fit bg-lighterdark shadow-2xl">
+              <div class="w-11/12 xl:w-3/6 mx-auto pt-48 text-center">
+                <p
+                  class="relative pb-8 text-center text-[1.1rem] text-white text-justify leading-5 z-10 select-none"
+                  dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(info) }}
+                ></p>
               </div>
             </div>
           </div>
 
-          <CollectionSearch font={font} myslug={mySlug} type="movement" />
+          <CollectionSearch font={font} myslug={slug} type="movement" />
         </div>
       </main>
 
