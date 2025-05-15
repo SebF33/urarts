88bc5dcb21bda1ca256b8c4315a8e35d3df9f862
SebF33 <78741537+SebF33@users.noreply.github.com>
2025-05-15 23:18:19 +0200

    colors update

--- utils/colors.ts
+++ utils/colors.ts
@@ -14,3 +14,77 @@ export const colorScheme = {
 };
 
 export const currentColorScheme = "everblush";
+
+export const extraColors = {
+  water: "#e0f7fa",
+};
+
+// Pour la carte du Monde
+export const worldColors = [
+  "#000000", //  0 Allemagne (noir)
+  "#FF0000", //  1 Armenia (rouge)
+  "#FF0000", //  2 Arménie (rouge)
+  "#ED2939", //  3 Austria (rouge autrichien)
+  "#ED2939", //  4 Autriche (rouge autrichien)
+  "#00662E", //  5 Belarus (vert)
+  "#000000", //  6 Belgique (noir)
+  "#000000", //  7 Belgium (noir)
+  "#00662E", //  8 Biélorussie (vert)
+  "#FF0000", //  9 Canada (rouge)
+  "#DE2910", // 10 China (rouge chinois)
+  "#DE2910", // 11 Chine (rouge chinois)
+  "#FCD116", // 12 Colombia (jaune)
+  "#FCD116", // 13 Colombie (jaune)
+  "#FFFFFF", // 14 Czechoslovakia (blanc)
+  "#C60C30", // 15 Danemark (rouge danois)
+  "#C60C30", // 16 Denmark (rouge danois)
+  "#FFC400", // 17 Espagne (jaune)
+  "#B22234", // 18 États-Unis d'Amérique (Old Glory Red)
+  "#003580", // 19 Finland (bleu finlandais)
+  "#003580", // 20 Finlande (bleu finlandais)
+  "#0055A4", // 21 France (bleu français)
+  "#000000", // 22 Germany (noir)
+  "#0D5EAF", // 23 Grèce (bleu grec)
+  "#0D5EAF", // 24 Greece (bleu grec)
+  "#CE1126", // 25 Hongrie (rouge hongrois)
+  "#CE1126", // 26 Hungary (rouge hongrois)
+  "#009246", // 27 Italie (vert italien)
+  "#009246", // 28 Italy (vert italien)
+  "#BC002D", // 29 Japan (rouge japonais)
+  "#BC002D", // 30 Japon (rouge japonais)
+  "#006847", // 31 Mexico (vert mexicain)
+  "#006847", // 32 Mexique (vert mexicain)
+  "#00AEEF", // 33 Monde (bleu « Terre »)
+  "#AE1C28", // 34 Netherlands (rouge néerlandais)
+  "#BA0C2F", // 35 Norvège (rouge norvégien)
+  "#BA0C2F", // 36 Norway (rouge norvégien)
+  "#AE1C28", // 37 Pays-Bas (rouge néerlandais)
+  "#FFFFFF", // 38 Poland (blanc)
+  "#FFFFFF", // 39 Pologne (blanc)
+  "#FF0000", // 40 Portugal (rouge portugais)
+  "#012169", // 41 Royaume-Uni (bleu Union Jack)
+  "#FFFFFF", // 42 Russia (blanc)
+  "#FFFFFF", // 43 Russie (blanc)
+  "#FFC400", // 44 Spain (jaune)
+  "#005BBB", // 45 Suède (bleu suédois)
+  "#FF0000", // 46 Suisse (rouge)
+  "#005BBB", // 47 Sweden (bleu suédois)
+  "#FF0000", // 48 Switzerland (rouge)
+  "#FFFFFF", // 49 Tchécoslovaquie (blanc)
+  "#0057B8", // 50 Ukraine (bleu ukrainien)
+  "#012169", // 51 United Kingdom (bleu Union Jack)
+  "#B22234", // 52 United States of America (Old Glory Red)
+  "#DA251D", // 53 Vietnam (rouge vietnamien)
+];
+
+export const tagColorsEN = [
+  "#A0A0A0", // 54 Architecture (gris architectural)
+  "#CE1126", // 55 Egypt (rouge égyptien)
+  "#228B22", // 56 Landscape (vert forêt)
+];
+
+export const tagColorsFR = [
+  "#A0A0A0", // 54 Architecture (gris architectural)
+  "#228B22", // 55 Paysage (vert forêt)
+  "#CE1126", // 56 Égypte (rouge égyptien)
+];
